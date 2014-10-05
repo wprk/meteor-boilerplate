b8255990499fb89efdc062f234a0b3d663fe8883
Will Parker <wprk14@gmail.com>
2014-10-05 14:45:11 +0100

    Fix user accounnt system

--- client/views/application/auth/login/login.js
+++ client/views/application/auth/login/login.js
@@ -39,13 +39,3 @@ Template.login.events({
     });
   }
 });
-
-Template.accountLoggedIn.events({
-  "click #logout": function (event) {
-    event.preventDefault();
-    Meteor.logout(function() {
-        Errors.throw('danger', 'You have been logged out.');
-        Router.go('login');
-    });
-  }
-});
