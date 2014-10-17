dc0205a93a10489220e09ca9d5353ffb9644a90b
Will Parker <Will.Parker@widerplan.com>
2014-10-17 17:20:48 +0100

    Password reset tweaks - still need to get url from email to work though

--- client/views/application/layouts/partials/header.js
+++ client/views/application/layouts/partials/header.js
@@ -2,7 +2,7 @@ Template.accountLoggedIn.events({
   "click #logout": function (event) {
     event.preventDefault();
     Meteor.logout(function() {
-        Errors.throw('danger', 'You have been logged out.');
+        Alerts.add('You have been logged out.', 'danger');
         Router.go('login');
     });
   }
